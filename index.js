9374a51949fe9779f8ea0f2d6c52ad450cb2c01b
Simon Cook <simon.cook@salesforce.com>
2016-10-10 18:46:24 +0100

    updated skill

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ var alexa = require( 'alexa-app' );
 var app = new alexa.app( 'salesforce-skill' );
 
 app.launch( function( request, response ) {
-	response.say( 'Welcome to your test skill' ).reprompt( 'Way to go. You got it to run. Bad ass.' ).shouldEndSession( false );
+	response.say( 'Welcome to your test number app' ).reprompt( 'Well done, you got it working!' ).shouldEndSession( false );
 } );
-
 
 app.error = function( exception, request, response ) {
 	console.log(exception)
